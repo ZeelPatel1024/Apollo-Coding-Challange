ecd85f826127cc627c57530b1616cc34947ce060
zpate6 <zpate6@uic.edu>
2025-12-04 07:48:44 -0600

    Set up Vehicle model

---
+++ controller/vehicleController.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const vehicleSchema = new mongoose.Schema({
+    manufacturer_name:{
+        type:String,
+        required: true
+    },
+    description:{
+        type:String,
+        required: true
+    },
+    horse_power:{
+        type:Number,
+        required: true
+    },
+    model_name:{
+        type:String,
+        required: true
+    },
+    model_year:{
+        type:Number,
+        required: true
+    },
+    purchase_price:{
+        type:Number,
+        required: true
+    },
+    fuel_type:{
+        type:String,
+        required: true
+    },
+})
+
+export default mongoose.model("Vehicle", vehicleSchema);
